f673a8f9f4f060cda279f177871d7bd1ac98cb19
MohamedAzahaf <mohamed.azahaf@student.hogent.be>
2026-05-17 18:35:52 +0200

    front-end bugs fixed

--- obj/Debug/net9.0/InventarisApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/InventarisApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventarisApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8241da26f27c4cc0c84923bc61bb5328081e53a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f085e26bc0d4a1d22cfd933359d9a55f3af98c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventarisApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventarisApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
